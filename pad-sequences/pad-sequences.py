687db866f3656d2911c9aae5cfd68d1aa3257cb4
Truc Thi-Thanh Le <143344728+funaLee@users.noreply.github.com>
2026-04-08 12:00:40 +0800

    Update solution for pad-sequences

--- pad-sequences/pad-sequences.py
+++ pad-sequences/pad-sequences.py
@@ -10,16 +10,10 @@ def pad_sequences(seqs, pad_value=0, max_len=None):
     N = len(seqs)
     L = max_len if (max_len != None) else max((len(seq) for seq in seqs), default=0)
 
-    pad_seq = np.zeros((N, L))
+    pad_seq = np.full((N, L), pad_value)
 
     for i, seq in enumerate(seqs):
-        seq_len = len(seq)
-        padding_needed = max(0, L - seq_len)
-        truncation_needed = max(0, seq_len - L)
-        
-        padding_array = np.array([pad_value] * padding_needed)
-        seq = seq[:-truncation_needed] if truncation_needed > 0 else seq
-
-        pad_seq[i] = np.concatenate((seq, padding_array))
+        valid_value = min(len(seq), L)
+        pad_seq[i, :valid_value] = seq[:valid_value]
 
     return pad_seq
